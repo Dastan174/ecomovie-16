8b442e65bd58edcca869d14561d044a6d19ec19e
serafim13M <kimkomte@gmail.com>
2025-12-10 00:20:48 +0600

    alihan

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import MainRoutes from "./routes/MainRoutes";
+import Header from "./components/layout/header/Header";
+import Footer from "./components/layout/footer/Footer";
 
 const App = () => {
-  return <MainRoutes />;
+  return (
+    <>
+      <Header />
+      <Footer />
+      <MainRoutes />
+    </>
+  );
 };
 
 export default App;
