5c6c41b9b15f653580852816dc5af81bfc7083b6
serafim13M <kimkomte@gmail.com>
2025-12-10 17:18:59 +0600

    header/footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import Header from "./components/layout/header/Header";
 import Footer from "./components/layout/footer/Footer";
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Footer />
-      <MainRoutes />
-    </>
-  );
+  return <MainRoutes />;
 };
 
 export default App;
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,7 +3,6 @@ import scss from "./header.module.scss";
 import { CiSearch } from "react-icons/ci";
 import { GiHamburgerMenu } from "react-icons/gi";
 
-
 export default function Header() {
   return (
     <header className={scss.container}>
@@ -11,7 +10,10 @@ export default function Header() {
         <div className={scss.mainContainer}>
           <div className={scss.header}>
             <div className={scss.left}>
-              <img src="https://movie.elcho.dev/assets/eco-movie-logo-a8_bjuTM.svg" alt="" />
+              <img
+                src="https://movie.elcho.dev/assets/eco-movie-logo-a8_bjuTM.svg"
+                alt=""
+              />
               {/* <span>EcoMovie</span> */}
             </div>
             <div className={scss.nigth}>
